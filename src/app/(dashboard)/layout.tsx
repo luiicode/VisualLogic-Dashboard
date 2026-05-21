c5c66c12ed9df1ee42be178013e4928544b0ae44
luiCode <luiseduardo.corte35@gmail.com>
2026-05-20 18:10:55 -0600

    build overview dashboard UI components

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -14,7 +14,9 @@ export default function DashboardLayout({
         <AppSidebar />
         <SidebarInset>
           <TopBar />
-          <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0 overflow-y-auto">
+            {children}
+          </div>
         </SidebarInset>
       </SidebarProvider>
     </TooltipProvider>
--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,24 @@
+import { ChartInteractive } from "@/components/custom/ChartInteractive";
+import { DataTable, schema } from "@/components/custom/DataTables";
+import { SectionCards } from "@/components/custom/SectionCard";
+import rawData from "@/mock/mock.data.overview.json";
+
+import { z } from "zod";
+
+const data = rawData as z.infer<typeof schema>[];
+
 export default function OverviewPage() {
   return (
-    <div>
-      <h1>Overview Page</h1>
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <SectionCards />
+          <div className="px-4 lg:px-6">
+            <ChartInteractive />
+          </div>
+          <DataTable data={data} />
+        </div>
+      </div>
     </div>
   );
 }
