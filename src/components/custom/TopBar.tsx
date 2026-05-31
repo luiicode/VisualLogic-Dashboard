dfe42e3b97cba976f7decedf804774726e329d42
luiCode <luiseduardo.corte35@gmail.com>
2026-05-31 17:25:25 -0600

    refactor: remove unused Breadcrumb and Separator components from TopBar

--- src/components/custom/TopBar.tsx
+++ src/components/custom/TopBar.tsx
@@ -1,12 +1,3 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "../ui/breadcrumb";
-import { Separator } from "../ui/separator";
 import { SidebarTrigger } from "../ui/sidebar";
 
 export const TopBar = () => {
@@ -14,21 +5,6 @@ export const TopBar = () => {
     <header className="flex h-16 shrink-0 items-center gap-2">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
-        <Separator
-          orientation="vertical"
-          className="mr-2 data-vertical:h-4 data-vertical:self-auto"
-        />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className="hidden md:block">
-              <BreadcrumbLink href="#">Build Your Application</BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className="hidden md:block" />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
       </div>
     </header>
   );
